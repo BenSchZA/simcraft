315ccf49080333f82fe79e5b5b62046c14ca1532
BenSchZA <BenSchZA@users.noreply.github.com>
2025-11-16 10:57:51 +0000

    Tree shaking for desktop build

--- frontend/src/lib/simcraft/index.ts
+++ frontend/src/lib/simcraft/index.ts
@@ -1,7 +1,6 @@
 export * from './base';
 
 import type { SimcraftAdapter } from './base';
-import { BrowserAdapter } from './browser';
 import { DesktopAdapter } from './desktop';
 
 const isDesktopBuild = import.meta.env.VITE_BUILD_TARGET === 'desktop';
@@ -9,4 +8,13 @@ const isDesktopBuild = import.meta.env.VITE_BUILD_TARGET === 'desktop';
 export const isDesktop = '__TAURI_INTERNALS__' in window || isDesktopBuild;
 export const isBrowser = !isDesktop;
 
-export const createAdapter: () => Promise<SimcraftAdapter | null> = async () => isDesktop ? new DesktopAdapter() : new BrowserAdapter();
+export const createAdapter: () => Promise<SimcraftAdapter | null> = async () => {
+	if (isDesktopBuild || isDesktop) {
+		console.log('Creating desktop adapter');
+		return new DesktopAdapter();
+	} else {
+		console.log('Creating browser adapter');
+		const { BrowserAdapter } = await import('./browser');
+		return new BrowserAdapter();
+	}
+};
